2a491961da1d239e46fdfbc51f93b1b85a4beebf
Ahmed Ramadan <elkholy_ahmed@outlook.com>
2020-03-19 11:55:01 +0200

    first commit

---
+++ resources/views/backend/index.blade.php
@@ -0,0 +1,22 @@
+@extends('backend.layouts.master')
+
+@section('title','')
+
+{{-- additional stylesheets --}}
+@section('stylesheet')
+    
+@endsection
+{{-- end additional stylesheets --}}
+
+{{-- content --}}
+@section('content')
+    
+@endsection
+{{-- end content --}}
+
+
+{{-- additional scripts --}}
+@section('stylesheet')
+    
+@endsection
+{{-- end additional scripts --}}
---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,7 @@
+@include('backend.components.head')
+@include('backend.components.menu')
+@include('backend.components.scripts')
+@include('backend.components.footer')
+
+
+@yield('content')
