2e483f3700e7828af683a6150f44c8fe9ce72f13
Ahmed Ramadan <elkholy_ahmed@outlook.com>
2020-03-25 19:35:54 +0200

    latest_commit

--- app/Http/Controllers/Backend/ProductsController.php
+++ app/Http/Controllers/Backend/ProductsController.php
@@ -22,10 +22,10 @@ use Hash;
 class ProductsController extends Controller
 {
     public $path = 'backend.pages.Products.';
-//    public function __construct()
-//    {
-//        $this->middleware('auth');
-//    }
+   public function __construct()
+   {
+       $this->middleware('auth');
+   }
 
     public function index()
     {
