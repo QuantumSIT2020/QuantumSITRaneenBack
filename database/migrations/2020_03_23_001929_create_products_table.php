4b6077b91d0819da2aa5f6e7790c0e189717647d
bian <bian@fahmawy.com>
2020-04-01 17:55:53 +0200

    another update

--- database/migrations/2020_03_23_001929_create_products_table.php
+++ database/migrations/2020_03_23_001929_create_products_table.php
@@ -19,16 +19,18 @@ class CreateProductsTable extends Migration
             $table->string('ar_name');
             $table->string('product_image');
             $table->text('description');
-            $table->text('video');
+            $table->text('video')->nullable();
             $table->integer('price');
             $table->integer('quantity');
             $table->boolean('isactive')->default(0); //0 not active - 1 active
-            $table->unsignedBigInteger('manufacturer_id');
+            $table->unsignedBigInteger('manufacturer_id')->nullable();
             $table->foreign('manufacturer_id')->references('id')->on('manufacturers')->onDelete('cascade');
             $table->unsignedBigInteger('sub_categories_id');
             $table->foreign('sub_categories_id')->references('id')->on('sub_categories')->onDelete('cascade');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
             $table->timestamps();
         });
     }
